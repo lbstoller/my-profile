fe5b8b698d848eefc9eeab8e46ca5a10b0f2f318
Leigh Stoller <lbstoller@gmail.com>
2016-12-13 07:26:22 -0700

    Upload new file

---
+++ profile.py
@@ -0,0 +1,22 @@
+"""An example of constructing a profile with a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
+
+Instructions:
+Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
+"""
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
