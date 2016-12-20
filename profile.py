01b1b981dabeba5cf3d8ae4fc7b84bad6d65e59b
Leigh Stoller <lbstoller@gmail.com>
2016-12-20 07:34:26 -0700

    Add a parameter to the profile to test switching back and forth between parameterized profile and plain profile.

--- profile.py
+++ profile.py
@@ -4,8 +4,6 @@ Instructions:
 Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
 """
 
-# This is a silly comment
-
 # Import the Portal object.
 import geni.portal as portal
 # Import the ProtoGENI library.
@@ -16,9 +14,29 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
+
+#
+# This is a typical list of images.
+#
+imageList = [
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD', 'UBUNTU 16.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU14-64-STD', 'UBUNTU 14.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTIS71-64-STD', 'CENTOS 7.1'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD102-64-STD', 'FreeBSD 10.2')]
+
+# Define a single parameter for the instantiation page.
+pc.defineParameter("osImage", "Select OS image",
+                   portal.ParameterType.IMAGE,
+                   imageList[0], imageList,
+                   longDescription="Most clusters have this set of images, " +
+                   "pick your favorite one.")
+
+# Retrieve the values the user specifies during instantiation.
+params = pc.bindParameters()
+
 # Add a raw PC to the request.
 node = request.RawPC("node")
+node.disk_image = params.osImage
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
